77af839fdfdd67caaebc3fc9efe11d1bb6189612
Tom Szymczuk <tszymczuk@student.bridgew.edu>
2021-10-19 10:46:17 -0400

    Calculator

---
+++ src/main/java/bsu/comp152/calctuesthurs/Calcwindow.java
@@ -0,0 +1,16 @@
+package bsu.comp152.calctuesthurs;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class Calcwindow {
+    @FXML
+    private TextField numberDisplay;
+
+
+    @FXML
+    public void numberButtonPushed(ActionEvent event){
+        
+    }
+}
