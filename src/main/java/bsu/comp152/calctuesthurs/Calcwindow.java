f31418921dc2c1eb32e5eb5ad2a6b352fdb35eab
Tom Szymczuk <tszymczuk@student.bridgew.edu>
2021-10-21 10:43:38 -0400

    Calculator Added with Add and Equal and Clear

--- src/main/java/bsu/comp152/calctuesthurs/Calcwindow.java
+++ src/main/java/bsu/comp152/calctuesthurs/Calcwindow.java
@@ -2,17 +2,41 @@ package bsu.comp152.calctuesthurs;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class Calcwindow {
+    private int storedNumber;
+
     @FXML
     private TextField numberDisplay;
-
 
     @FXML
     public void numberButtonPushed(ActionEvent event){
         var currentText = numberDisplay.getText();
-        var currentNumberText = event.getSource().toString();
+        var ButtonPressed = (Button)event.getSource();
+        var currentNumberText = ButtonPressed.getText();
         var newText = currentText+currentNumberText;
+        numberDisplay.setText(newText);
+    }
+
+    @FXML
+    public void addButtonPressed() {
+        var numberText = numberDisplay.getText();
+        storedNumber = Integer.parseInt(numberText);
+        numberDisplay.clear();
+    }
+
+    @FXML
+    public void equalButtonPressed(){
+        var currentNumberText = numberDisplay.getText();
+        var currentNumber = Integer.parseInt(currentNumberText);
+        var sum = currentNumber + storedNumber;
+        numberDisplay.setText(""+sum);
+    }
+
+    @FXML
+    public void clearButtonPressed() {
+        numberDisplay.setText("");
     }
 }
---
+++ src/main/java/bsu/comp152/calctuesthurs/OperatorType.java
@@ -0,0 +1,8 @@
+package bsu.comp152.calctuesthurs;
+
+public enum OperatorType {
+    Add,
+    Subtract,
+    Multiply,
+    Divide;
+}
