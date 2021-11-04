5f85869cb455f875bb12150f6cafca245b8d56f8
Tom Szymczuk <tszymczuk@student.bridgew.edu>
2021-11-04 09:46:46 -0400

    Calculator complete

--- src/main/java/bsu/comp152/calctuesthurs/Calcwindow.java
+++ src/main/java/bsu/comp152/calctuesthurs/Calcwindow.java
@@ -7,32 +7,64 @@ import javafx.scene.control.TextField;
 
 public class Calcwindow {
     private int storedNumber;
-
+    private OperatorType lastOp;
     @FXML
     private TextField numberDisplay;
 
     @FXML
-    public void numberButtonPushed(ActionEvent event){
+    public void numberButtonPushed(ActionEvent event) {
         var currentText = numberDisplay.getText();
-        var ButtonPressed = (Button)event.getSource();
+        var ButtonPressed = (Button) event.getSource();
         var currentNumberText = ButtonPressed.getText();
-        var newText = currentText+currentNumberText;
+        var newText = currentText + currentNumberText;
         numberDisplay.setText(newText);
     }
 
     @FXML
-    public void addButtonPressed() {
+    public void subtractButtonPressed() {
         var numberText = numberDisplay.getText();
         storedNumber = Integer.parseInt(numberText);
         numberDisplay.clear();
+        lastOp = OperatorType.Subtract;
     }
 
     @FXML
-    public void equalButtonPressed(){
+    public void multiplyPressed() {
+        var numberText = numberDisplay.getText();
+        storedNumber = Integer.parseInt(numberText);
+        numberDisplay.clear();
+        lastOp = OperatorType.Multiply;
+    }
+
+    @FXML
+    public void dividePressed() {
+        var numberText = numberDisplay.getText();
+        storedNumber = Integer.parseInt(numberText);
+        numberDisplay.clear();
+        lastOp = OperatorType.Divide;
+    }
+
+    @FXML
+    public void addButtonPressed() {
+        var numbertext = numberDisplay.getText();
+        storedNumber = Integer.parseInt(numbertext);
+        numberDisplay.clear();
+        lastOp = OperatorType.Add;
+    }
+
+    @FXML
+    public void equalButtonPressed() {
+        double result = 0.0;
         var currentNumberText = numberDisplay.getText();
         var currentNumber = Integer.parseInt(currentNumberText);
-        var sum = currentNumber + storedNumber;
-        numberDisplay.setText(""+sum);
+        switch (lastOp) {
+            case Add -> result = currentNumber + storedNumber;
+            case Subtract -> result = storedNumber - currentNumber;
+            case Multiply -> result = currentNumber * storedNumber;
+            case Divide -> result = storedNumber / currentNumber;
+        }
+
+        numberDisplay.setText("" + result);
     }
 
     @FXML
